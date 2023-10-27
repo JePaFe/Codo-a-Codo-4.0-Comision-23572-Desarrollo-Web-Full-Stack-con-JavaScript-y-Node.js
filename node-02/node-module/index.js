820029cb3da488e8b90c3333e59082e089c8bd82
Jean Paul Ferreira <jeanpaul@jepafe.net>
2023-10-27 14:24:00 -0300

    25. Servidor Web con Node JS Nativo

---
+++ node-02/node-module/index.js
@@ -0,0 +1,5 @@
+const sumar = require("./modulo");
+
+const suma = sumar(2, 3);
+
+console.log("La suma es: " + suma);
---
+++ node-02/node-module/modulo.js
@@ -0,0 +1,5 @@
+const sumar = (num1, num2) => {
+  return num1 + num2;
+};
+
+module.exports = sumar;
